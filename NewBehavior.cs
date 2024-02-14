cbd154fbf0b740f55224080b9bd244800981b0bf
rvb3115 <rvb3115@gmail.com>
2024-02-13 21:37:55 -0600

    Everything updated to date with the old version

--- NewBehavior.cs
+++ NewBehavior.cs
@@ -2,8 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehavior : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
+    public GameObject Player1;
+    public GameObject destination;
+    public float speed;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,6 @@ public class NewBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       Player1.transform.position = Vector3.MoveTowards(Player1.transform.position, destination.transform.position, speed); 
     }
 }
--- Player1Controller.cs
+++ Player1Controller.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Player1Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   private Player thePlayer;
+   public TextMeshPro tm;
 
-    // Update is called once per frame
-    void Update()
+    void Start()// Start is called before the first frame update
     {
-        
+    this.thePlayer = new Player("Brady");
+    tm.text = this.thePlayer.getName() + " -> " + this.thePlayer.getHP();
+    
+    
+    }
+   
+
+    private void FixedUpdate() //locked at 60fps  // Update is called once per frame
+    {
+       this.thePlayer.display();
+       
+
     }
 }
