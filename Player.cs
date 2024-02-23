1b91744f05e95a91ac3a63e407afa2a0553a4db5
rvb3115 <rvb3115@gmail.com>
2024-02-23 10:11:52 -0600

    updated

--- Player.cs
+++ Player.cs
@@ -9,6 +9,7 @@ public class Player : MonoBehaviour
     public GameObject southExit;
     public GameObject eastExit;
     public GameObject westExit;
+    public GameObject middleOfTheRoom;
     private float speed = 5.0f;
     private bool amMoving = false;
     private bool amAtMiddleOfRoom = false;
@@ -33,42 +34,95 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Rigidbody rb = this.gameObject.GetComponent<Rigidbody>();
+
         //disable all exits when the scene first loads
         this.turnOffExits();
 
-        //not our first scene
+        //disable the middle collider until we know what our initial state will be
+        //it should already be disabled by default, but for clarity, lets do it here
+        this.middleOfTheRoom.SetActive(false);
+
         if (!MySingleton.currentDirection.Equals("?"))
         {
-            if(MySingleton.currentDirection.Equals("north"))
+            //mark ourselves as moving since we are entering the scene through one of the exits
+            this.amMoving = true;
+
+            //we will be positioning the player by one of the exits so we can turn on the middle collider
+            this.middleOfTheRoom.SetActive(true);
+            this.amAtMiddleOfRoom = false;
+
+            if (MySingleton.currentDirection.Equals("north"))
             {
                 this.gameObject.transform.position = this.southExit.transform.position;
+                this.gameObject.transform.LookAt(this.northExit.transform.position);
+                //rb.MovePosition(this.southExit.transform.position);
             }
             else if (MySingleton.currentDirection.Equals("south"))
             {
                 this.gameObject.transform.position = this.northExit.transform.position;
+                this.gameObject.transform.LookAt(this.southExit.transform.position);
+                //rb.MovePosition(this.northExit.transform.position);
             }
             else if (MySingleton.currentDirection.Equals("west"))
             {
                 this.gameObject.transform.position = this.eastExit.transform.position;
+                this.gameObject.transform.LookAt(this.westExit.transform.position);
+                //rb.MovePosition(this.eastExit.transform.position);
             }
             else if (MySingleton.currentDirection.Equals("east"))
             {
                 this.gameObject.transform.position = this.westExit.transform.position;
+                this.gameObject.transform.LookAt(this.eastExit.transform.position);
+                //rb.MovePosition(this.westExit.transform.position);
             }
+            //StartCoroutine(turnOnMiddle());
+        }
+        else
+        {
+            //We will be positioning the play at the middle
+            //so keep the middle collider off for this run of the scene
+            this.amMoving = false;
+            this.amAtMiddleOfRoom = true;
+            this.middleOfTheRoom.SetActive(false);
+            this.gameObject.transform.position = this.middleOfTheRoom.transform.position;
         }
     }
 
+    /*
+    IEnumerator turnOnMiddle()
+    {
+        yield return new WaitForSeconds(1);
+        this.middleOfTheRoom.SetActive(true);
+        print("turned on");
+
+    }
+    */
+
     private void OnTriggerEnter(Collider other)
     {
+        print(other.tag);
         if(other.CompareTag("door"))
         {
+            print("Loading scene");
+
             EditorSceneManager.LoadScene("DungeonRoom");
         }
         else if(other.CompareTag("middleOfTheRoom") && !MySingleton.currentDirection.Equals("?"))
         {
-            
+            //we have hit the middle of the room, so lets turn off the collider
+            //until the next run of the scene to avoid additional collisions
+            this.middleOfTheRoom.SetActive(false);
+            this.turnOnExits();
+
+            print("middle");
             this.amAtMiddleOfRoom = true;
+            this.amMoving = false;
             MySingleton.currentDirection = "middle";
+        }
+        else
+        {
+            print("spomethilskdfjskldjfsdjkl");
         }
     }
 
